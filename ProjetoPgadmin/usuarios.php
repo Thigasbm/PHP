92d4df5f1924fa72d17840bfedbde79325f75d07
Thigasbm <129339926+Thigasbm@users.noreply.github.com>
2023-05-25 21:55:02 -0300

    Add files via upload

---
+++ ProjetoPgadmin/usuarios.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catálogo de Serviços</title>
+</head>
+<body>
+     Usuarios:<br><br>
+    <div>
+    <label for="nome">Nome:</label>
+    <input type="text" id=><br>
+    <label for="sobrenome">Sobrenome:</label>
+    <input type="text" id=><br>
+    <label for="email">E-mail</label>
+    <input type="text" id=><br>
+     <label for="senha">Senha</label>
+    <input type="text" id=><br>
+    <label for="telefone">Telefone:</label>
+    <input type="text" id=><br>
+    <label for="cpf_cnpj">Cpf/Cnpj</label>
+    <input type="text" id=><br>
+    <label for="data_nasc">Data de Nascimento:</label>
+    <input type="text" id=><br>
+    <label for="cep">Cep:</label>
+    <input type="text" id=><br>
+    <label for="endereco">Endereço:</label>
+    <input type="text" id=><br>
+    <label for="numero">Número:</label>
+    <input type="text" id=><br>
+    <label for="rua">Rua</label>
+    <input type="text" id=><br>
+     <label for="complemento">Complemento:</label>
+    <input type="text" id=><br>
+     Sexo: <select name="sexo">
+            <option value="M" selected>Masculino</option>
+            <option value="F">Feminino</option>
+            <option value="O">Outro</option>
+           </select><br>
+
+           <a href="index.php"><button>Voltar</button></a><br>
+
+</body>
+</html>
