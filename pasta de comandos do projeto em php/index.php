7a9dab0f823783382b347f76063c1a25505cb19d
Thigasbm <129339926+Thigasbm@users.noreply.github.com>
2023-06-07 18:01:47 -0300

    Add files via upload

---
+++ pasta de comandos do projeto em php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catálogo de Serviços</title>
+</head>
+<body>
+    <a href="usuarios.php"><button>Cadastro</button></a>
+
+</body>
+</html>
---
+++ pasta de comandos do projeto em php/listaUsuarios.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Usuários</title>
+</head>
+
+<body>
+    <?php
+    $consulta = $_GET["consulta"];
+
+    if ($consulta = "Sim"):
+        @$conexao = pg_connect("host=localhost port=5432 dbname=GestaoDeTrabalhos user=postgres  password=JovemP*2023");//conexão
+        pg_set_client_encoding($conexao, 'UNICODE');
+    endif;
+    ?>
+
+    <table width="100%" border="1">
+        <thead>
+            <th>NOME</th>
+            <th>CIDADE</th>
+            <th>EMAIL</th>
+            <th colspan="2">AÇÕES</th>
+        </thead>
+        <tdbody>
+            <?php
+            $select = "SELECT usuarios.id_usuario, usuarios.nome, cidades.nome, usuarios.email
+            FROM usuarios INNER JOIN cidades ON usuarios.id_cidade = cidades.id_cidade
+            ORDER BY usuarios.nome";//colocar mais campos aqui
+
+            $resultado = pg_query($conexao, $select);
+            while ($linha = pg_fetch_array($resultado)){//pedir o que é pg_fetch_array
+                $idusuario = $linha[0];
+                $nomeusuario = $linha[1];
+                $cidadeusuario = $linha[2];
+                $emailusuario = $linha[4];
+            ?>
+        <tr>
+            <td><?php echo $nomeusuario;?></td>
+            <td><?php echo $cidadeusuario;?></td>
+            <td><?php echo $emailusuario;?></td>
+            <td width="10%" align="center"><a href="#" onclick="AcaoUsuario(<?php echo $idusuario;?>, 'EX')">EXCLUIR</a></td>
+            <td width="10%" align="center"><a href="#" onclick="AcaoUsuario(<?php echo $idusuario;?>, 'ED')">EDITAR</a></td>
+        </tr>
+                <?php
+            }
+            ?>
+        <tdbody>
+    </table>
+</body>
+</html>
